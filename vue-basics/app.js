3b9cd8a6bd3510e3fbe15dac1561fff0a890a4f3
TedEngineer <tedvu184@gmail.com>
2023-02-05 21:24:42 +1100

    Add more explanation on the feature

--- vue-basics/app.js
+++ vue-basics/app.js
@@ -2,6 +2,7 @@ const app = Vue.createApp({
   data() {
     // always return an object
     // now the the data can be used in html where vue controls
+    // this concept is called data interpolation
     return {
       courseGoal: "Master Vue",
     };
