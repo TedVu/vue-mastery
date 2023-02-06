9d293eb19fce3fa437193190510f5db17c51e2ba
TedEngineer <tedvu184@gmail.com>
2023-02-06 21:36:44 +1100

    Add methods in vue

--- vue-basics/app.js
+++ vue-basics/app.js
@@ -8,6 +8,16 @@ const app = Vue.createApp({
       vueLink: "https://vuejs.org/",
     };
   },
+  methods: {
+    printGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return "Learn Vue!";
+      } else {
+        return "Master Vue!";
+      }
+    },
+  },
 });
 
 app.mount("#user-goal");
