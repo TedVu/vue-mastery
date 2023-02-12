4756c185bbe2671a93d26e34171075cd1c7bcbf0
TedEngineer <tedvu184@gmail.com>
2023-02-13 10:43:10 +1100

    Add header component

---
+++ vue-crashcourse/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <h1> Task Tracker </h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderComponent',
+    }
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
