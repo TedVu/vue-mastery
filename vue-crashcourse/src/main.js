11e57d51943d34c8b92529c6c95dcd78c817961b
TedEngineer <tedvu184@gmail.com>
2023-02-19 18:29:59 +1100

    Remove redundant code and finish deploying to netlify

--- vue-crashcourse/src/main.js
+++ vue-crashcourse/src/main.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+console.log(`The current environment is`, process.env.NODE_ENV);
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
